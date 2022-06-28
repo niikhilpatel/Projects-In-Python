48d1a8766ea4b15b3f792c2771d8bc58b9bb73fa
Nikhil Patel <90297144+niikhilpatel@users.noreply.github.com>
2022-06-28 21:30:09 +0530

    Create Defang Ip address file

---
+++ Defang IP Address/Defang_ip_address.py
@@ -0,0 +1,12 @@
+import ipaddress
+from tkinter.ttk import Separator
+
+
+def ip_address(address):
+    new_address =""
+    split_address = address.split(".")
+    separator = "[.]"
+    new_address = separator.join(split_address)
+    return new_address
+ipaddress = ip_address("1.1.2.3")    
+print(ip_address)
