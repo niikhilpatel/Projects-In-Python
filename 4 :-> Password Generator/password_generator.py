fea6a2cac008cfffc91cd4461a200d2e3bd943cc
Nikhil Patel <90297144+niikhilpatel@users.noreply.github.com>
2022-06-30 22:02:52 +0530

    update password_generator file

--- 4 :-> Password Generator/password_generator.py
+++ 4 :-> Password Generator/password_generator.py
@@ -1,1 +1,9 @@
 import random
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+Symbols = "[]{}()*;/,_-"
+all = lower + upper + numbers + Symbols
+length = 16
+password = ''''.join(random.sample(all,length))
+print(password)
